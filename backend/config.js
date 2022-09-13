90136e978b04d41e620bd43a40a02258df4b622f
Adarsh220499 <katigiadarsh@gmail.com>
2022-09-12 22:28:16 -0400

    adding mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://admin:admin@bookstore-mern.jxtk530.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore-MERN'
